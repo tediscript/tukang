7c2fa205714530482dcfa1482d4f66839e9ec6ab
tediscript <tediscript@gmail.com>
2011-06-08 00:22:03 +0700

    rm dong

--- application/views/form.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php echo site_url(); ?>/upload/do_upload" method="POST" enctype="multipart/form-data">
-    <input type="file" name="userfile" value="" />
-    <input type="submit" value="upload"/>
-</form>
